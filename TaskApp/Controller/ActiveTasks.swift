fabbb417a7b9779e61f4c17277adf3353de7033e
Ion Mardari <john.mardari@gmail.com>
2018-07-03 19:12:26 -0400

    Add functionality to Activity screen to add and remove taks (localy)

--- TaskApp/Controller/ActiveTasks.swift
+++ TaskApp/Controller/ActiveTasks.swift
@@ -7,11 +7,85 @@
 //
 
 import UIKit
+import Firebase
 
 class ActiveTasks: UITableViewController {
-
+    
+    // MARK: Properties
+    var tasks: [Task] = []
+    
+    let ref = Database.database().reference(withPath: "active-tasks")
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ActiveCell", for: indexPath)
+        let task = tasks[indexPath.row]
+        
+        cell.textLabel?.text = task.name
+        
+        toggleCellCheckbox(cell, isCompleted: task.completed)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tasks.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        var task = tasks[indexPath.row]
+        let toggledCompletion = !task.completed
+        
+        toggleCellCheckbox(cell, isCompleted: toggledCompletion)
+        task.completed = toggledCompletion
+        tableView.reloadData()
+    }
+    
+    func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
+        if !isCompleted {
+            cell.accessoryType = .none
+            cell.textLabel?.textColor = .black
+        } else {
+            cell.accessoryType = .checkmark
+            cell.textLabel?.textColor = .gray
+        }
+    }
+    
+    // MARK:- Add Items
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "New Task", message: "Add a New Task", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Add", style: .default) { _ in
+            // Grab whatever user typed in to the text field
+            let textField = alert.textFields![0]
+            // Assign that value as the task name
+            let task = Task(name: textField.text!, completed: false)
+            // Apend the task to the array of tasks
+            self.tasks.append(task)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
--- TaskApp/Model/Task.swift
+++ TaskApp/Model/Task.swift
@@ -14,14 +14,12 @@ struct Task {
     let ref: DatabaseReference?
     let key: String
     let name: String
-    let addedByUser: String
-    let completed: Bool
+    var completed: Bool
     
-    init(name: String, addedByUser: String, completed: Bool, key: String = "") {
+    init(name: String, completed: Bool, key: String = "") {
         self.ref = nil
         self.key = key
         self.name = name
-        self.addedByUser = addedByUser
         self.completed = completed
     }
     
@@ -29,7 +27,6 @@ struct Task {
         guard
             let value = snapshot.value as? [String: AnyObject],
             let name = value["name"] as? String,
-            let addedByUser = value["addedByUser"] as? String,
             let completed = value["completed"] as? Bool else {
                 return nil
         }
@@ -37,14 +34,12 @@ struct Task {
         self.ref = snapshot.ref
         self.key = snapshot.key
         self.name = name
-        self.addedByUser = addedByUser
         self.completed = completed
     }
     
     func toAnyObject() -> Any {
         return [
             "name": name,
-            "addedByUser": addedByUser,
             "completed": completed
         ]
     }
