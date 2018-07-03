7c279d2dda71d140e9d1ca103ed327ddb8e9b828
Ion Mardari <john.mardari@gmail.com>
2018-07-02 20:26:42 -0400

    Implement the Task Model

---
+++ TaskApp/Model/TaskModel.swift
@@ -0,0 +1,51 @@
+//
+//  TaskModel.swift
+//  TaskApp
+//
+//  Created by Ion M on 7/2/18.
+//  Copyright © 2018 Ion M. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Task {
+    
+    let ref: DatabaseReference?
+    let key: String
+    let name: String
+    let addedByUser: String
+    let completed: Bool
+    
+    init(name: String, addedByUser: String, completed: Bool, key: String = "") {
+        self.ref = nil
+        self.key = key
+        self.name = name
+        self.addedByUser = addedByUser
+        self.completed = completed
+    }
+    
+    init?(snapshot: DataSnapshot) {
+        guard
+            let value = snapshot.value as? [String: AnyObject],
+            let name = value["name"] as? String,
+            let addedByUser = value["addedByUser"] as? String,
+            let completed = value["completed"] as? Bool else {
+                return nil
+        }
+        
+        self.ref = snapshot.ref
+        self.key = snapshot.key
+        self.name = name
+        self.addedByUser = addedByUser
+        self.completed = completed
+    }
+    
+    func toAnyObject() -> Any {
+        return [
+            "name": name,
+            "addedByUser": addedByUser,
+            "completed": completed
+        ]
+    }
+}
